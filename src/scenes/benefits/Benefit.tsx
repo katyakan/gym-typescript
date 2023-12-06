cadd0d29d19199e2c68c480f38ba7360efe846c3
katyakan <katyakan86@gmail.com>
2023-12-06 13:37:04 +0600

    add animation to benefits

--- src/scenes/benefits/Benefit.tsx
+++ src/scenes/benefits/Benefit.tsx
@@ -2,6 +2,12 @@ import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
+const childVariant = {
+  hidden: {opacity: 0, scale: 0.9},
+  visible: {opacity: 1, scale: 1}
+
+}
+
 type Props = {
   icon: JSX.Element;
   title: string;
